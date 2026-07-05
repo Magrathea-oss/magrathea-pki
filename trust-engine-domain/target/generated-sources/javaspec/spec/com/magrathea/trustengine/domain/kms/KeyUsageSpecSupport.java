13c0ccfa1e4edf37d8efb525a9ecc0e8bb0e3609
Mario Giustiniani <mariogiustiniani@gmail.com>
2026-07-05 18:51:14 +0200

    chore: add .gitignore, remove PLAN.md and target/ from tracking

--- trust-engine-domain/target/generated-sources/javaspec/spec/com/magrathea/trustengine/domain/kms/KeyUsageSpecSupport.java
+++
@@ -1,9 +0,0 @@
-package spec.com.magrathea.trustengine.domain.kms;
-
-import com.magrathea.trustengine.domain.kms.KeyUsage;
-
-public class KeyUsageSpecSupport extends io.github.jvmspec.api.ObjectBehavior<KeyUsage> {
-    public KeyUsageSpecSupport() {
-        super(KeyUsage.class);
-    }
-}
