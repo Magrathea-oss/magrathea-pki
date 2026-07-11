ddbbd081ad372570730832f6e734527928278dda
Mario Giustiniani <mariogiustiniani@gmail.com>
2026-07-11 18:23:32 +0200

    feat(x509): expose stable algorithm wire tokens

--- trust-engine-domain/src/main/java/com/magrathea/trustengine/x509/profile/PublicKeyAlgorithm.java
+++ trust-engine-domain/src/main/java/com/magrathea/trustengine/x509/profile/PublicKeyAlgorithm.java
@@ -1,6 +1,16 @@
 package com.magrathea.trustengine.x509.profile;
 
 public enum PublicKeyAlgorithm {
-    EC_P256,
-    RSA_3072
+    EC_P256("EC_P256"),
+    RSA_3072("RSA_3072");
+
+    private final String wireToken;
+
+    private PublicKeyAlgorithm(String wireToken) {
+        this.wireToken = wireToken;
+    }
+
+    public String wireToken() {
+        return wireToken;
+    }
 }
